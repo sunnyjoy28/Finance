b95760a1cfa83a666aabe6d667ce20ff08d5acae
sunnyjoy28 <sunnysabharwal2233@gmail.com>
2025-11-21 05:11:40 +0530

    Delete finance-dashboard-package/finance-dashboard directory

--- finance-dashboard-package/finance-dashboard/backend/models/Bill.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const billSchema = new mongoose.Schema({
-  name: String,
-  dueDate: Date,
-  amount: Number,
-  status: { type: String, enum: ['Scheduled','Paid','Overdue'], default: 'Scheduled' }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Bill', billSchema);
